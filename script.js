29c207057305e2a98adfbbe5506add053fa62d5f
shivendra-somr <shivendra.somr@gmail.com>
2023-07-10 23:45:26 +0530

    link updated

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function openInNewWindow(link) {
     window.open(link, '_blank');
-  }
+}
 
-  function downloadResume(link) {
-    var link = document.createElement('a');
-    link.href = 'link';
-    link.download = 'resume.pdf';
-    link.click();
-  }
+function downloadResume() {
+    var linkResume = document.createElement('a');
+    linkResume.href = "https://drive.google.com/uc?export=download&id=1R8r7h2asT6xN01Vpa0_iIQjvFT5w5sTW";
+    linkResume.download = 'resume.pdf';
+    linkResume.click();
+}
