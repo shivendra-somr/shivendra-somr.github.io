c1a01581622676f4b270104231b624bdbec3eb65
shivendra-somr <shivendra.somr@gmail.com>
2023-07-10 23:20:10 +0530

    resume download link added

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function openInNewWindow(link) {
     window.open(link, '_blank');
   }
+
+  function downloadResume(link) {
+    var link = document.createElement('a');
+    link.href = 'link';
+    link.download = 'resume.pdf';
+    link.click();
+  }
